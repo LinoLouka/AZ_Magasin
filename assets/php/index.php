5ebe1feceb1c975aed7116dfc4f7ae1b3564fb90
L0uka <loukacadaulino@gmail.com>
2023-06-08 11:01:10 +0200

    add img

--- assets/php/index.php
+++ assets/php/index.php
@@ -15,7 +15,7 @@
     <a href="">Product</a>
     <a href="">Contact</a>
   </div>
-  <img src="/assets/img/shopping-cart.svg" alt="panier d'achat">
+  <img src="https://raw.githubusercontent.com/LinoLouka/AZ_Magasin/e54b9a2a1be2b3549c48fc6b1b3580afaf46cb18/assets/shopping-cart.svg" alt="panier d'achat">
   <p>Login</p>
 </header>
 
@@ -24,51 +24,34 @@
     <div id="top">
       <h1>SHOE THE RIGHT ONE.</h1>
       <input type="submit" value="See our store">
-      <img src="/assets/img/shoe_one.png" alt="chaussure nike1">
+      <img src="https://github.com/LinoLouka/AZ_Magasin/blob/main/assets/shoe_one.png?raw=true" alt="chaussure nike1">
+
     </div>
     <section id="display_last-products">
       <h2>Our last products</h2>
       <div id="last-products">
-        <img src="/assets/img/shoe_one.png" alt="chaussure nike1">
-        <p>NIKE Air</p>
-        <p>234€</p>
-        <input type="submit" value="Add to card">
-      </div>
-      <div id="last-products">
-        <img src="/assets/img/shoe_one.png" alt="chaussure nike1">
-        <p>NIKE Air</p>
-        <p>234€</p>
-        <input type="submit" value="Add to card">
-      </div>
-      <div id="last-products">
-        <img src="/assets/img/shoe_one.png" alt="chaussure nike1">
-        <p>NIKE Air</p>
-        <p>234€</p>
-        <input type="submit" value="Add to card">
-      </div>
-      <div id="last-products">
-        <img src="/assets/img/shoe_one.png" alt="chaussure nike1">
+        <img src="https://github.com/LinoLouka/AZ_Magasin/blob/main/assets/shoe_one.png?raw=true" alt="chaussure nike1">
         <p>NIKE Air</p>
         <p>234€</p>
         <input type="submit" value="Add to card">
       </div>
     </section>
     <div id="bottom">
-      <img src="/assets/img/shoe_two.png" alt="chaussure nike2">
+      <img src="https://github.com/LinoLouka/AZ_Magasin/blob/main/assets/shoe_two.png?raw=true" alt="chaussure nike2">
       <h2>WE PROVIDE YOU THE BEST QUALITY.</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia quisquam molestiae temporibus, voluptas commodi iste exercitationem nisi eveniet maxime, porro doloribus dolor voluptates, fuga quibusdam est? Fugit unde impedit asperiores.</p>
     </div>
     <section id="user-reviews">
       <div id="emily">
-        <img src="/assets/img/image-emily.jpg" alt="photo d'Emily">
+        <img src="https://github.com/LinoLouka/AZ_Magasin/blob/main/assets/image-emily.jpg?raw=true" alt="photo d'Emily">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam provident, tenetur culpa earum maiores adipisci nobis asperiores impedit? Distinctio, repudiandae veniam. Dolorem laborum, quia doloremque animi asperiores temporibus modi necessitatibus!</p>
       </div>
       <div id="thomas">
-        <img src="/assets/img/image-thomas.jpg" alt="photo de Thomas">
+        <img src="https://github.com/LinoLouka/AZ_Magasin/blob/main/assets/image-thomas.jpg?raw=true" alt="photo de Thomas">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, provident aspernatur? Perspiciatis ab dolor adipisci tempore itaque cum, aliquid voluptate magni, sequi debitis nostrum necessitatibus qui ipsa similique nihil rerum.</p>
       </div>
       <div id="jennie">
-        <img src="/assets/img/image-jennie.jpg" alt="photo de Jennie">
+        <img src="https://github.com/LinoLouka/AZ_Magasin/blob/main/assets/image-jennie.jpg?raw=true" alt="photo de Jennie">
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit suscipit numquam cum officiis eveniet, mollitia voluptas? Quia totam perferendis, eius facere ipsam ducimus autem similique hic, vitae dignissimos inventore distinctio.</p>
       </div>
     </section>
